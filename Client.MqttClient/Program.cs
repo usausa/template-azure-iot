d518b03be41fd7b222878fb96530b83e4f3f4dc9
usausa <machi.pon@gmail.com>
2022-07-10 17:09:56 +0900

    Add project

---
+++ Client.MqttClient/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Client.MqttClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# Template project for Azure
